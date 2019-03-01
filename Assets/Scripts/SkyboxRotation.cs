26987d3a7e47c69cd2b1fbdcc6b2f32efbdd60cb
Eric Mouledoux <EricMouledoux@gmail.com>
2019-03-01 09:26:24 -0600

    all teleporting should be linked

---
+++ Assets/Scripts/SkyboxRotation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyboxRotation : MonoBehaviour
+{
+    public Material skyboxMaterialDay;
+    public Material skyboxMaterialNight;
+
+    // Update is called once per frame
+    void Update()
+    {
+        RenderSettings.skybox.SetFloat("_Rotation", (Time.time % 360f));
+    }
+}
