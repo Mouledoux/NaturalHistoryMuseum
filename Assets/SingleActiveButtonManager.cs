05974ec4ca72988f57350339787af2de81b6ee2d
Eric Mouledoux <EricMouledoux@gmail.com>
2019-03-07 16:28:03 -0600

    demo for interior only

---
+++ Assets/SingleActiveButtonManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingleActiveButtonManager : MonoBehaviour
+{
+    private List<UnityEngine.UI.Button> m_managedButtons = new List<UnityEngine.UI.Button>();
+    
+	void Start ()
+    {
+        foreach (UnityEngine.UI.Button button in GetComponentsInChildren<UnityEngine.UI.Button>())
+        {
+            button.onClick.AddListener(()=> UpdateButtons(button));
+            m_managedButtons.Add(button);
+        }
+
+        m_managedButtons[0].onClick.Invoke();
+        UpdateButtons(m_managedButtons[0]);
+	}
+
+    private void UpdateButtons(UnityEngine.UI.Button activeButton)
+    {
+        foreach (UnityEngine.UI.Button button in GetComponentsInChildren<UnityEngine.UI.Button>())
+        {
+            button.interactable = true;
+        }
+
+        activeButton.interactable = false;
+    }
+}
