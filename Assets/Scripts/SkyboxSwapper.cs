e6df82fbfa4fd41547f32d0dec01ab3c4c526a3b
@rootAccess <EricMouledoux@Gmail.com>
2019-03-12 09:16:44 -0500

    moved scripts to new folder
    added sky swapper script as well

---
+++ Assets/Scripts/SkyboxSwapper.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyboxSwapper : MonoBehaviour
+{
+    public bool m_random = false;
+    public Material[] m_skyboxes;
+    private static int m_currentSkyboxIndex = 0;
+
+    void Start()
+    {
+        RenderSettings.skybox =
+            m_random ?
+            m_skyboxes[Random.Range(0, m_skyboxes.Length)] :
+            m_skyboxes[(++m_currentSkyboxIndex % m_skyboxes.Length)];
+    }
+}
