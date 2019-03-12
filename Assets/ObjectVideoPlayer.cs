d364c61a3d4cb591ff020ab719d389006522a47a
@rootAccess <EricMouledoux@Gmail.com>
2019-03-12 15:55:34 -0500

    object video player script

---
+++ Assets/ObjectVideoPlayer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectVideoPlayer : MonoBehaviour
+{
+    [SerializeField]
+    private UnityEngine.Video.VideoPlayer videoPlayer;
+
+    public UnityEngine.Events.UnityEvent OnVideoFinish;
+
+    void Start()
+    {
+        
+    }
+
+
+    public void PlayVideo(float delay = 0f)
+    {
+        StartCoroutine(iPlayVideo(delay));
+    }
+    
+    public IEnumerator iPlayVideo(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        videoPlayer.Play();
+
+        yield return new WaitWhile(() => videoPlayer.isPlaying);
+
+        OnVideoFinish.Invoke();
+    }
+}
