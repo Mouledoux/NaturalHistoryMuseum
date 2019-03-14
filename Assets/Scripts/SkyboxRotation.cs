1965610d272cc42e660956cbe1acc7f17f6462fd
@rootAccess <EricMouledoux@Gmail.com>
2019-03-14 10:48:05 -0500

    more space skyboxes

--- Assets/Scripts/SkyboxRotation.cs
+++ Assets/Scripts/SkyboxRotation.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class SkyboxRotation : MonoBehaviour
 {
+    [SerializeField, Range(0f, 1f)]
+    float speed = 1f;
+
     // Update is called once per frame
     void Update()
     {
-        RenderSettings.skybox.SetFloat("_Rotation", (Time.time % 360f));
+        RenderSettings.skybox.SetFloat("_Rotation", (Time.time % 360f) * speed);
     }
 }
