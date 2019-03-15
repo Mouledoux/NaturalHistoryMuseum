ebbdf24b6843d0d7946b7ab20b74043ce2396258
@rootAccess <EricMouledoux@Gmail.com>
2019-03-15 11:36:38 -0500

    fixed owl display case scale

---
+++ Assets/Scripts/RandomRotation.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class RandomRotation : MonoBehaviour
+{
+    [Range(1f, 10f)]
+    public float rotSpeed;
+    public Vector3 rotScale = Vector3.one;
+
+    void Update()
+    {
+        Vector3 rot = new Vector3(Random.Range(0f, 1f) * rotScale.x, Random.Range(0f, 1f) * rotScale.y, Random.Range(0f, 1f) * rotScale.z) * rotSpeed * Time.deltaTime;
+        transform.Rotate(rot, Space.Self);
+    }
+}
